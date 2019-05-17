944c243c93bd738c4a5b0ea80bfe08d29b2145a1
Khamhuang, David <khameye19888@gmail.com>
2019-05-17 12:00:04 -0500

    working on functionallity

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+var total = Math.floor(Math.random() * 102 + 19);
+
+
+
+
+
+
+
+function(){
+    $("#goal").html(<b>total</b>
+console.log (total)
+        )};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
