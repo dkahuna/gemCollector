266c0dd967f5f476054f514b51edea359bd5f113
Khamhuang, David <khameye19888@gmail.com>
2019-05-18 23:33:58 -0500

    final touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
+var won = 0;
+var lost = 0;
 var crystalVal = 0;
 var quest = Math.floor(Math.random() * 102 + 19);
 
 //console.log(crystalVal)
 
+
+//printing the objective
 $("#goal").text("KARATS: " + quest);
+
+
+
 //function karats (){
     //var quest = Math.floor(Math.random() * 102 + 19);
    // crystalVal.push(quest)};
@@ -13,12 +20,15 @@ $("#goal").text("KARATS: " + quest);
 
 
 
+// image values
 
 var unoBear = Math.floor(Math.random() * 12 + 1);
 $(".bear").on("click", function(){
   
   crystalVal =  crystalVal + unoBear;
   $("#final").text(crystalVal);
+  referee ()
+
   console.log (unoBear)
 })
 
@@ -27,6 +37,8 @@ $(".bear").on("click", function(){
     
     crystalVal =  crystalVal + unoBuffalo;
     $("#final").text(crystalVal);
+    referee ()
+ 
     console.log (unoBuffalo)
   })
 
@@ -35,6 +47,8 @@ $(".bear").on("click", function(){
     
     crystalVal =  crystalVal + unoWolf;
     $("#final").text(crystalVal);
+    referee ()
+   
     console.log (unoWolf)
   })
   
@@ -43,8 +57,37 @@ $(".bear").on("click", function(){
     
     crystalVal =  crystalVal + unoTrex;
     $("#final").text(crystalVal);
+    referee ()
+  
     console.log (unoTrex)
   })
+
+//add value to the wins&losses
+
+  function referee () {
+      if (crystalVal === quest){
+          won = won+1;
+          $("#up").text("Wins: " + won);
+}   else if (crystalVal >quest){
+    lost = lost+1;
+    $("#down").text("Losses: " + lost);
+  return reset ()
+}
+     
+// cant get it not to count a loss after winning the game in order for it to start a new game again
+function reset () {
+    quest = Math.floor(Math.random() * 102 + 19);
+    $("#goal").text(quest);
+    crystalVal = 0;
+    $("#final").text(crystalVal);
+    unoBear= Math.floor(Math.random() * 12 + 1);
+    unoBuffalo= Math.floor(Math.random() * 12 + 1);
+    unoWolf= Math.floor(Math.random() * 12 + 1);
+    unoTrex = Math.floor(Math.random() * 12 + 1);
+ 
+  };
+
+
    // alert("works" + crystalVal);
 
 
@@ -57,5 +100,4 @@ $(".bear").on("click", function(){
 //}
 //$(".bear").on("click", function() {});
     
-
-;
+  };
