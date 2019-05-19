2648051fdaa35dfe249659333ab5c81718c6244c
Khamhuang, David <khameye19888@gmail.com>
2019-05-18 22:25:16 -0500

    pic value function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 var crystalVal = 0;
 var quest = Math.floor(Math.random() * 102 + 19);
+
 //console.log(crystalVal)
 
 $("#goal").text("KARATS: " + quest);
@@ -13,13 +14,48 @@ $("#goal").text("KARATS: " + quest);
 
 
 
-
+var unoBear = Math.floor(Math.random() * 12 + 1);
 $(".bear").on("click", function(){
-    crystalVal += 20; 
-    alert("works" + crystalVal);
-});
+  
+  crystalVal =  crystalVal + unoBear;
+  $("#final").text(crystalVal);
+  console.log (unoBear)
+})
+
+  var unoBuffalo = Math.floor(Math.random() * 12 + 1);
+  $(".unoBuffalo").on("click", function(){
+    
+    crystalVal =  crystalVal + unoBuffalo;
+    $("#final").text(crystalVal);
+    console.log (unoBuffalo)
+  })
+
+  var unoWolf = Math.floor(Math.random() * 12 + 1);
+  $(".unoWolf").on("click", function(){
+    
+    crystalVal =  crystalVal + unoWolf;
+    $("#final").text(crystalVal);
+    console.log (unoWolf)
+  })
+  
+  var unoTrex = Math.floor(Math.random() * 12 + 1);
+  $(".unoTrex").on("click", function(){
+    
+    crystalVal =  crystalVal + unoTrex;
+    $("#final").text(crystalVal);
+    console.log (unoTrex)
+  })
+   // alert("works" + crystalVal);
 
 
+//if (crystalVal === quest) {
+//    alert("winner");
+//}
+
+//else if (crystalVal>= quest ) {
+  //  alert("loser!");
+//}
 //$(".bear").on("click", function() {});
     
 
+;
