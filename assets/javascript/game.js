2b72b53f816681e9905945cd7932279012b06c2f
Khamhuang, David <khameye19888@gmail.com>
2019-05-18 19:12:36 -0500

    printed and generated rndm num

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,25 @@
-var crystalVal = []
-console.log(crystalVal)
-
-
-function karats (){
-    var quest = Math.floor(Math.random() * 102 + 19);
-    crystalVal.push(quest)
-};
-
-
-
-//$(".btn").on("click", karats())
+var crystalVal = 0;
+var quest = Math.floor(Math.random() * 102 + 19);
 //console.log(crystalVal)
 
-$(".bear").on("click", function() {
-alert("Please Work")
+$("#goal").text("KARATS: " + quest);
+//function karats (){
+    //var quest = Math.floor(Math.random() * 102 + 19);
+   // crystalVal.push(quest)};
+
+
+//console.log (quest)
+
+
+
+
+
+$(".bear").on("click", function(){
+    crystalVal += 20; 
+    alert("works" + crystalVal);
 });
+
+
+//$(".bear").on("click", function() {});
+    
+
